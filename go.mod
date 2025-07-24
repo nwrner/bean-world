45fa6b124e647c26131d7e92d2e9720cafaba5d8
Nick Warner <nwrner516@gmail.com>
2025-07-24 13:03:35 -0600

    Initial commit: Hello World Go app with test

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/nwrner/bean-world
+
+go 1.24.5
---
+++ main_test.go
@@ -0,0 +1,8 @@
+package main
+
+import "testing"
+
+func TestMainPrint(t *testing.T) {
+    // Normally we would test a function, but for now:
+    t.Log("This is just a placeholder test!")
+}
